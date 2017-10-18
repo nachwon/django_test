b66bb460351ddb42ed8da885e8a81e0a5d844f6b
Che1 <nachwon@gmail.com>
2017-10-18 11:59:52 +0900

    minor fixes and css added

--- django_test/config/settings.py
+++ django_test/config/settings.py
@@ -15,10 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-print(MEDIA_ROOT)
 MEDIA_URL = '/media/'
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 
 # Quick-start development settings - unsuitable for production
