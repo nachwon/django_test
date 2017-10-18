8983cb3ffb97938279c2190f09129f93e990ad28
Che1 <nachwon@gmail.com>
2017-10-18 10:30:34 +0900

    Pillow installed, Post model ready to go

--- django_test/blog/models.py
+++ django_test/blog/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    photo = models.ImageField()
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
